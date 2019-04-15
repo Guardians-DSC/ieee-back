4c9f24bab43e5b5c460c182eb50cecd78b80ec8d
nathan <nathanfpedroza@gmail.com>
2019-04-14 23:58:59 -0300

    Task 1 concluída, aguardo avaliação

--- src/task/task.model.js
+++ src/task/task.model.js
@@ -1,25 +1,57 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-
+    /**
+     * Nome da atividade
+     */
     name : {
         type: String,
         default: "",
         required: true,
     },
-
-    description: {
+    /**
+     * Tipo da atividade
+     */
+    type: {
         type: String,
         default: "",
         required: true,
     },
-
-    hours: {
+    /**
+     * Descrição opcional
+     * Edit: Mudado para FALSE, visto que é opcional
+     */
+    description: {
+        type: String,
+        default: "",
+        required: false,
+    },
+    /**
+     * Data de realização
+     */
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    /**
+     * Carga horária
+     */
+    //Renomeei Hours para workload (carga horária), pra não haver ambiguidades.
+    workload: {
         type: Number,
         default: 0,
         required: true,
     },
 
+    /**
+     * Horario em que foi realizado.
+     */
+    time: {
+        type: String,
+        default: "00:00:00",
+        requird : true,
+    }
     /*
     TODO: Relacionar as entidades
     */
