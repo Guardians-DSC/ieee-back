09c0bfbc2b7a570c29197c90d70fa451cdfc9dff
Yovany Cunha <yovany33@gmail.com>
2019-06-20 09:20:50 -0300

    Update express.js

--- config/express.js
+++ config/express.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 module.exports = function () {
     const app = express();
     
-    app.set('port', process.env.PORT || 3000);
+    app.set('port', process.env.PORT || 8080);
     app.use(cors());
     app.use(bodyParser.urlencoded({
         extended: true,
