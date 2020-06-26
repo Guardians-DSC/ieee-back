417b93d41952d793092fb7812238826a594544ce
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2020-06-26 00:19:06 -0300

    Finish refact

--- src/task/task.service.js
+++ src/task/task.service.js
@@ -35,7 +35,6 @@ const taskService = (function ()  {
   const _getAllTasks = async function (callback) {
     try {
       Task.find({}, function (err, tasks) {
-        console.log(err);
         if (err) 
           return callback(response.notFound('Não foi possível recuperar as atividades.'));
         else 
